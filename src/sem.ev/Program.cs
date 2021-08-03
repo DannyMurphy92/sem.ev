e0c1366b0ded61df2b578577e8d09c8f4521fa69
Danny Murphy <Danny.Murphy@decision.tech>
2021-08-03 19:19:51 +0100

    Initial commit

---
+++ src/sem.ev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sem.ev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
