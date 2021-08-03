a6396c22afe6c792ff48eea24ccdc8068cac5724
Danny Murphy <Danny.Murphy@decision.tech>
2021-08-03 22:24:14 +0100

    Implement console app

--- src/sem.ev/Program.cs
+++ src/sem.ev/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using sem.ev.services;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace sem.ev
 {
@@ -6,7 +9,35 @@ namespace sem.ev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arithmeticSvc = new ArithmeticService();
+            var numbers = new List<double>();
+
+            using (var reader = new StreamReader(@".\data\SampleData.xls"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var lineValues = line.Split(',');
+
+                    for (int i = 0; i < lineValues.Length; i++)
+                    {
+                        if (double.TryParse(lineValues[i], out var value))
+                        {
+                            numbers.Add(value);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"Mean of the data is {arithmeticSvc.Mean(numbers.ToArray())}");
+            Console.WriteLine($"The standard deviation of the data is {arithmeticSvc.StandardDeviation(numbers.ToArray())}");
+
+            foreach(var resp in arithmeticSvc.BucketData(numbers.ToArray(), 10))
+            {
+                Console.WriteLine($"{resp.bucket}: {resp.entries}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
